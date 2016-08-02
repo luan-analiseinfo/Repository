07b9e233df7c921cbf8b21e7107b5e394f4e93e6
Luan Lucas <luan.analise@outlook.com>
2016-08-01 21:07:43 -0300

    Criação e relacionamento OneToOne

---
+++ br.com.project.checkskills/src/main/java/br/com/project/checkskills/entities/dadosbasicos/FuncionarioEntity.java
@@ -0,0 +1,46 @@
+package br.com.project.checkskills.entities.dadosbasicos;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import br.com.project.checkskills.entities.autenticacao.UsuarioEntity;
+import br.com.project.checkskills.utils.BaseEntity;
+
+@Entity
+@Table(name="TB_FUNCIONARIO")
+@AttributeOverride(name = "id", column = @Column(name = "ID_FUNCIONARIO"))
+public class FuncionarioEntity extends BaseEntity<Long> {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	private String nome;
+	
+	@OneToOne(cascade = CascadeType.ALL, optional = false, fetch = FetchType.EAGER, orphanRemoval = true)
+	private UsuarioEntity usuarioEntity;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public UsuarioEntity getUsuarioEntity() {
+		return usuarioEntity;
+	}
+
+	public void setUsuarioEntity(UsuarioEntity usuarioEntity) {
+		this.usuarioEntity = usuarioEntity;
+	}
+	
+	
+
+}
