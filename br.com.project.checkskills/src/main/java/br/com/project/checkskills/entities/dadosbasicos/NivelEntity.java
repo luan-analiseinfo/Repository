f2432b38f45e5566348a1e79b9fc64c5735373d3
Luan Lucas <luan.analise@outlook.com>
2016-08-02 22:25:18 -0300

    Revert "Entidade Nivel | repository | teste -->> funconando"
    This reverts commit d70cb82df3891ab568911017ae0cb7124adf33ac.

--- br.com.project.checkskills/src/main/java/br/com/project/checkskills/entities/dadosbasicos/NivelEntity.java
+++ br.com.project.checkskills/src/main/java/br/com/project/checkskills/entities/dadosbasicos/NivelEntity.java
@@ -1,29 +1,5 @@
 package br.com.project.checkskills.entities.dadosbasicos;
 
-import javax.persistence.AttributeOverride;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+public class NivelEntity {
 
-import br.com.project.checkskills.utils.BaseEntity;
-
-@Entity
-@Table
-@AttributeOverride(name="id", column=@Column(name = "ID_NIVEL"))
-public class NivelEntity extends BaseEntity<Long>  {
-
-	private static final long serialVersionUID = 1L;
-
-	@Column(name="nivel", nullable=false,length=45)
-	private String nivel;
-	
-	
-	public String getNivel() {
-		return nivel;
-	}
-	
-	public void setNivel(String nivel) {
-		this.nivel = nivel;
-	}
 }
-
